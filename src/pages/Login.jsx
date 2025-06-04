4b731757590208d2280eb85aa065c3356d17555a
Pramodya <pramodyapiyumanthi@gmail.com>
2025-06-04 10:47:42 +0530

    Add footer and browse by location

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Footer from "../components/Footer/Footer.jsx";
 
 function Login() {
   const [email, setEmail] = useState('');
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import EventList from "../pages/Home"
+import HomePage from "../pages/HomePage"
 import FilterEvents from "../pages/FilterEvents/FilterEvents"
 import EventDetail from "../pages/EventDetails"
 import Login from "../pages/Login"
@@ -21,7 +21,7 @@ import EventsSummary from "../pages/OrganizerDashboard/EventsSummary"
 
 
 export const routes = [
-  {path:'/',element:<EventList/>},
+  {path:'/',element:<HomePage/>},
   {path:'/find-events',element:<FilterEvents/>},
   {path:'/events/:id',element:<EventDetail/>},
   { path: '/login', element: <Login /> },
